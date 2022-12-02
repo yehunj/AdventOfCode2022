6ec71d0bb503f55f7f72519e5a87ee3d46ef3bcb
yehunj <jooyx@dukes.jmu.edu>
2022-12-02 00:49:13 -0500

    Day 2 Part 2

--- Day 2/day2.rb
+++ Day 2/day2.rb
@@ -1,4 +1,4 @@
-def main()
+def part1()
     input = File.read("input.txt").split("\n")
     score = 0
     input.each do |line|
@@ -30,4 +30,37 @@ def main()
     end
     puts score
 end
-main()
+
+
+def part2()
+    input = File.read("input.txt").split("\n")
+    score = 0
+    input.each do |line|
+        if line[0] == "A"
+            if line[2] == "X"
+                score += 3
+            elsif line[2] == "Y"
+                score += 4
+            elsif line[2] == "Z"
+                score += 8
+            end
+        elsif line[0] == "B"
+            if line[2] == "X"
+                score += 1
+            elsif line[2] == "Y"
+                score += 5
+            elsif line[2] == "Z"
+                score += 9
+            end
+        elsif line[0] == "C"
+            if line[2] == "X"
+                score += 2
+            elsif line[2] == "Y"
+                score += 6
+            elsif line[2] == "Z"
+                score += 7
+            end
+        end
+    end
+    puts score
+end
